c1eecce3e8f3e18ff501d95bb45f9097d463d827
Miguel Palacio <miguel.moyano@soy.sena.edu.co>
2023-06-08 18:58:26 -0500

    Elimnacion Proyecto CRUD por incorrecta generacion

--- Entregables IV Trimestre/Aplicación Web Jucar Autopartes/JucarAutopartesWebApplication/JucarAutopartesWebApplication/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace JucarAutopartesWebApplication.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
