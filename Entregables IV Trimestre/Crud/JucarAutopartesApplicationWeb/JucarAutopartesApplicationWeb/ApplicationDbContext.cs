69b14a9c1aa2e9fead4d7a70e5fd0b56d9327a5e
Miguel Palacio <miguel.moyano@soy.sena.edu.co>
2023-06-10 14:26:28 -0500

    Remodelamiento del Modelo Lógico

---
+++ Entregables IV Trimestre/Crud/JucarAutopartesApplicationWeb/JucarAutopartesApplicationWeb/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace JucarAutopartesApplicationWeb
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions options)
+            :base(options)
+        {
+                
+        }
+    }
+}
---
+++ Entregables IV Trimestre/Crud/JucarAutopartesApplicationWeb/JucarAutopartesApplicationWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JucarAutopartesApplicationWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
