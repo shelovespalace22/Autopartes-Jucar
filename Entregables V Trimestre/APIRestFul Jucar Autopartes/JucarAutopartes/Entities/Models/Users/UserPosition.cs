1bfad013f092af02823ea9a96e0e1546ba70a12d
shelovespalace22 <miguel.moyano@soy.sena.edu.co>
2023-08-31 19:41:43 -0500

    conf(Backend): Modelos de Users

---
+++ Entregables V Trimestre/APIRestFul Jucar Autopartes/JucarAutopartes/Entities/Models/Users/UserPosition.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models.Users
+{
+    public class UserPosition
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int UserPositionID { get; set; }
+
+        [Required]
+        [DisplayName("Creación del Regístro")]
+        public DateTime CreationDate { get; set; }
+
+        [Required]
+        [DisplayName("Modificación del Regístro")]
+        public DateTime ModificationDate { get; set; }
+
+
+        //Constructor
+
+        public UserPosition()
+        {
+            CreationDate = DateTime.Now;
+            ModificationDate = DateTime.Now;
+        }
+
+        //Relaciones con otros modelos 
+
+        //User
+        //public int UserID { get; set; }
+        public User? User { get; set; }
+
+        //Position
+        public int PositionID { get; set; }
+        public Position? Position { get; set; }
+    }
+}
