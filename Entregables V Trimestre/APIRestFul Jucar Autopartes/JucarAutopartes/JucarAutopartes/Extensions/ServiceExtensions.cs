9bc1f15d052e266c6bede5ef6a870b89379c10fa
shelovespalace22 <miguel.moyano@soy.sena.edu.co>
2023-09-05 19:02:44 -0500

    conf(Backend): Negociación de Contenido para Category

--- Entregables V Trimestre/APIRestFul Jucar Autopartes/JucarAutopartes/JucarAutopartes/Extensions/ServiceExtensions.cs
+++ Entregables V Trimestre/APIRestFul Jucar Autopartes/JucarAutopartes/JucarAutopartes/Extensions/ServiceExtensions.cs
@@ -37,5 +37,8 @@ namespace JucarAutopartes.Extensions
             services.AddDbContext<RepositoryContext>(opts =>
                 opts.UseSqlServer(configuration.GetConnectionString("sqlConnection")));
 
+        public static IMvcBuilder AddCustomCSVFormatter(this IMvcBuilder builder) =>
+            builder.AddMvcOptions(config => config.OutputFormatters.Add(new CsvOutputFormatter()));
+
     }
 }
