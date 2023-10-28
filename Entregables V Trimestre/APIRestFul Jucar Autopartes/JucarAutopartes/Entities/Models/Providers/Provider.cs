707b4d3777865b1056fc6f6ff883b0ea0cd955fa
shelovespalace22 <miguel.moyano@soy.sena.edu.co>
2023-10-28 17:46:05 -0500

    conf(Backend): Remodelaciones en Providers, se eliminó Factories.

--- Entregables V Trimestre/APIRestFul Jucar Autopartes/JucarAutopartes/Entities/Models/Providers/Provider.cs
+++ Entregables V Trimestre/APIRestFul Jucar Autopartes/JucarAutopartes/Entities/Models/Providers/Provider.cs
@@ -15,6 +15,26 @@ namespace Entities.Models.Providers
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid ProviderID { get; set; }
 
+        [Required(ErrorMessage = "Ingrese su Tipo de Identificación (NIT, C.C o C.E)")]
+        [MaxLength(50)]
+        [RegularExpression("^[A-Za-z.\\s]+$\r\n")]
+        [DisplayName("Tipo de Identifiación")]
+        public string? IdentifierType { get; set; }
+
+        [Required(ErrorMessage = "Ingrese su Número de Identificación")]
+        [MaxLength(10)]
+        [RegularExpression("^[0-9]+$\r\n")]
+        [DisplayName("Número de Identificación")]
+        public string IdentifierNumber { get; set; }
+
+        [Required(ErrorMessage = "Ingrese su Nombre o Razón Social")]
+        [MaxLength(100)]
+        [RegularExpression("^[A-Za-z.\\s]+$\r\n")]
+        [DisplayName("Nombre")]
+        public string? Name { get; set; }
+
+        /* Agregué tres campos nuevos, IdentifierType, IdentifierNumber y Name */
+
         [Required(ErrorMessage = "¡Ingrese un correo electrónico!")]
         [EmailAddress(ErrorMessage = "¡Ingrese una cuenta de correo electrónico válida!")]
         [DisplayName("Correo Electrónicos")]
@@ -44,6 +64,13 @@ namespace Entities.Models.Providers
         {
             State = true;
             CreationDate = DateTime.Now;
+            ModificationDate = DateTime.Now;
+        }
+
+        /* Métodos */
+
+        public void setModificationDate()
+        {
             ModificationDate = DateTime.Now;
         }
 
